3cb7db5d56cac57eac6e3f32efbb2d9eb9cff37e
Aman <aman.ug21@nsut.ac.in>
2022-10-25 18:37:05 +0530

    Commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "74024385fda8c6c8"; }
+/******/ 		__webpack_require__.h = function() { return "2064edfe8790b387"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
