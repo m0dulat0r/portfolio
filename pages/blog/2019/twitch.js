d066ad8ef6afef5a9de76e26a18c8779e0323d7e
Aman <aman.ug21@nsut.ac.in>
2022-10-31 20:03:11 +0530

    Update twitch.js

--- pages/blog/2019/twitch.js
+++ pages/blog/2019/twitch.js
@@ -1,4 +1,4 @@
-wimport styles from '/styles/Home.module.css'
+import styles from '/styles/Home.module.css'
 import Footer from "components/footer"
 
 import Heading from 'components/blogposts/heading'
